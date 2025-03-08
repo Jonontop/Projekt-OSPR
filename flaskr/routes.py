921050ecb4b255d39d752f413383f4aa07a97482
jonontop <jon.pecar@gmail.com>
2025-03-08 19:02:10 +0100

    changed name template to templates, changed css of nav to universal css file named: menu.css added routes for cpanel login about and main site fixed __init__.py

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,1 +1,18 @@
-from flask import render_template, Flask
+from flask import render_template
+from flaskr import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/cpanel')
+def cpanel():
+    return render_template('cpanel.html')
